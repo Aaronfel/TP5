80c234fb7163001b914d9924faf6dd3daed9e273
Aaron <feldmanaaron406@gmail.com>
2020-10-28 01:49:59 -0300

    tp5 realizado

---
+++ src/components/MainColors.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="container">
+			<Square 
+				class="square" 
+        :index="index"
+				:mensajes="mensajes"
+				:square="sq"
+				@e-square="eSquare"
+				:set-all-colors-to="setAllColorsTo"
+				:picked-color="pickedColor"
+				v-for="(sq,index) in squares"
+				:key="index"
+			/>
+		</div>
+
+</template>
+
+<script>
+
+  import Square from './Square'
+
+  export default  {
+    name: 'src-components-main-colors',
+    props: [
+      'squares',
+      'mensajes',
+      'pickedColor',
+      'setAllColorsTo'
+    ],
+    components: {
+      Square
+    },
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      eSquare(index,color) {
+        //console.log(index,color)
+        this.$emit('e-square',index,color)
+      }
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,46 @@
+<template>
+    <div :style="square" @click="squareClick()"></div>
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-square',
+    props: [
+      'index',
+      'square', 
+      'mensajes',
+      'pickedColor',
+      'setAllColorsTo'
+    ],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      squareClick(){
+        var clickedColor = this.square.backgroundColor;
+        if (clickedColor === this.pickedColor) {
+          this.mensajes.f.messageDisplay("You Picked Right!");
+          this.setAllColorsTo(this.pickedColor);
+          this.mensajes.f.restartButton("Play Again!");
+          this.mensajes.f.headerColor(this.pickedColor);
+        } else {
+          this.$emit('e-square',this.index,"#232323")
+          this.mensajes.f.messageDisplay("Try Again!");
+        }
+      }
+    },
+    computed: {
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+</style>
